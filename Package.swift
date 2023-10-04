cf068f51e8a5ce7ce04b23991b210961f062a934
1Conan <me@1conan.com>
2023-10-05 03:42:22 +0800

    switch to TextsHQ fork

--- Package.swift
+++ Package.swift
@@ -133,7 +133,7 @@ let package = Package(
         .library(name: "NIOHTTPCompression", targets: ["NIOHTTPCompression"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.42.0"),
+        .package(url: "https://github.com/TextsHQ/swift-nio.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: targets
